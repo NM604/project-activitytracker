c794da9036619d2a032ebe75ea0970bf121d100a
Nikhil Abraham Mathew <nikhilmath99@gmail.com>
2022-03-30 07:29:38 +0530

    Fixes missing link for top bar

--- planner/plan.py
+++ planner/plan.py
@@ -135,7 +135,7 @@ def thisday():
   this_day = request.args.get("d",1)
   this_month = request.args.get("m",1)
   this_year = request.args.get("y",1)
-  if this_day='01' and this_month='01' and this_year='1':
+  if this_day=='01' and this_month=='01' and this_year=='1':
     this_date = d
   user_id = session.get("userid")    
   this_date = str(this_year)+'-'+this_month+'-'+this_day
